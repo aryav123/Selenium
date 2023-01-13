062b455ddc334839d8eeeea3adb3285c9d8d24ad
aryav123 <arya74331@gmail.com>
2023-01-13 19:27:32 +0530

    (Initial commit)

---
+++ src/test/java/seleniumBasics/SeleniumCommands.java
@@ -0,0 +1,15 @@
+package seleniumBasics;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SeleniumCommands {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://demo.guru99.com/test/newtours/");
+		driver.close();
+	}
+
+}
